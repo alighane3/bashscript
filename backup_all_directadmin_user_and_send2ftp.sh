f0f23d512505fad9ddc25b56533013c724ded3f4
ali ghane <82435370+alighane3@users.noreply.github.com>
2025-02-17 14:18:22 +0330

    log added

--- backup_all_directadmin_user_and_send2ftp.sh
+++ backup_all_directadmin_user_and_send2ftp.sh
@@ -6,19 +6,36 @@ FTP_PASS="your ftp password"
 
 LOCAL_BACKUP_DIR="/backup-temp"
 FTP_BASE_DIR="/full-daily"
-
+LOG_FILE="/root/scripts/backup-full-to-ftp.log"
+CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 DAY_OF_WEEK=$(date +%A)
 FTP_TARGET_DIR="$FTP_BASE_DIR/$DAY_OF_WEEK"
 
+echo "-----------------------------"
+echo "[$CURRENT_TIME] Backup process started" | tee -a "$LOG_FILE"
+echo "[$CURRENT_TIME] Backup for day: $DAY_OF_WEEK in ftp directory $FTP_TARGET_DIR" | tee -a "$LOG_FILE"
+
 /usr/local/directadmin/directadmin admin-backup --destination=$LOCAL_BACKUP_DIR
 
-ftp -inv $FTP_SERVER <<EOF
-user $FTP_USER $FTP_PASS
-cd $FTP_TARGET_DIR
-mdelete *
-lcd $LOCAL_BACKUP_DIR
-mput *
-bye
+{
+    echo "[$(date "+%Y-%m-%d %H:%M:%S")] Connecting to FTP server $FTP_SERVER"
+
+    ftp -inv $FTP_SERVER <<EOF
+    user $FTP_USER $FTP_PASS
+    cd $FTP_TARGET_DIR
+    mdelete *
+    lcd $LOCAL_BACKUP_DIR
+    mput *
+    bye
 EOF
 
-rm -rf $LOCAL_BACKUP_DIR/*
+    echo "[$(date "+%Y-%m-%d %H:%M:%S")] File transfer to FTP completed"
+
+    # Delete local backup files after successful transfer
+    rm -rf $LOCAL_BACKUP_DIR/*
+    echo "[$(date "+%Y-%m-%d %H:%M:%S")] Local backup files deleted"
+} | tee -a "$LOG_FILE"
+
+
+CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+echo "[$CURRENT_TIME] Backup process completed" | tee -a "$LOG_FILE"
